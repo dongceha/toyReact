851c541a0686c2ffdfcb5fb211c01fcd6843dffc
dongceha <524970335@qq.com>
2020-08-02 16:03:08 +0800

    增加 setState

--- ToyReact.js
+++ ToyReact.js
@@ -3,15 +3,33 @@ class ElementWrapper {
         this.root = document.createElement(type)
     }
     setAttribute(name, value) {
-        this.root.setAttribute(name, value)
+        if (name.match(/^on([\s\S]+)$/)) {
+            let eventName = RegExp.$1.replace(/^[\s\S]/, (s) => s.toLowerCase())
+            this.root.addEventListener(eventName, value)
+        } else {
+            if (name === 'className')
+                name = 'class'
+            this.root.setAttribute(name, value)
+        }
     }
     // 把子节点挂载到当前的节点
     appendChild(vchild) {
-        vchild.mountTo(this.root)
+        // vchild.mountTo(this.root)
+        let range = document.createRange()
+        if (this.root.children.length) {
+            range.setStartAfter(this.root.lastChild)
+            range.setEndAfter(this.root.lastChild)
+        } else {
+            range.setStart(this.root, 0);
+            range.setEnd(this.root, 0)
+        }
+        vchild.mountTo(range)
     }
     // 所有的组件都会有 一个 mountTo
-    mountTo(parent) {
-        parent.appendChild(this.root)
+    mountTo(range) {
+        range.deleteContents()
+        range.insertNode(this.root)
+        // parent.appendChild(this.root)
     }
 }
 
@@ -19,31 +37,71 @@ class TextWrapper {
     constructor(content) {
         this.root = document.createTextNode(content)
     }
-    mountTo(parent) {
-        parent.appendChild(this.root)
+    mountTo(range) {
+        // parent.appendChild(this.root)
+
+        range.deleteContents()
+        range.insertNode(this.root)
     }
 }
 
 export class Component {
     constructor() {
         this.children = []
+        this.props = Object.create(null)
     }
-    mountTo(parent) {
+    mountTo(range) {
+        this.range = range
+        this.update()
+    }
+    update() {
+        // ====== 弄个占位，防止 下面删除了之后，range 失效
+        let placeholder = document.createComment('placeholder')
+        let range = document.createRange()
+        range.setStart(this.range.endContainer, this.range.endOffset)
+        range.setEnd(this.range.endContainer, this.range.endOffset)
+        range.insertNode(placeholder)
+        // ======
+
+        // 删除之前的内容
+        this.range.deleteContents()
         let vnode = this.render()
-        vnode.mountTo(parent)
+        vnode.mountTo(this.range)
+
+        // placeholder.parentNode.removeChild(placeholder)
     }
     setAttribute(name, value) {
+        this.props[name] = value
         this[name] = value
     }
     // 把 组件的 子节点收集起来，
     appendChild(vchild) {
         this.children.push(vchild)
     }
+    setState(state) {
+        // 把两个 state merge 起来
+        let merge = (oldState, newState) => {
+            for (let p in newState) {
+                if (typeof newState[p] === 'object') {
+                    if (typeof oldState[p] !== 'object') {
+                        oldState[p] = {}
+                    }
+                    merge(oldState[p], newState[p])
+                } else {
+                    oldState[p] = newState[p]
+                }
+            }
+        }
+        if (!this.state && state) {
+            this.state = {}
+        }
+        merge(this.state, state)
+        this.update()
+    }
 }
 
 export let ToyReact = {
     createElement(type, attributes, ...children) {
-        console.log(arguments)
         let element
         if (typeof type === 'string') {
             element = new ElementWrapper(type)
@@ -77,6 +135,14 @@ export let ToyReact = {
         return element
     },
     render(vdom, element) {
-        vdom.mountTo(element)
+        let range = document.createRange()
+        if (element.children.length) {
+            range.setStartAfter(element.lastChild)
+            range.setEndAfter(element.lastChild)
+        } else {
+            range.setStart(element, 0);
+            range.setEnd(element, 0)
+        }
+        vdom.mountTo(range)
     }
 }
--- main.js
+++ main.js
@@ -1,22 +1,60 @@
 // console.log('hell')
 import {ToyReact, Component} from './ToyReact'
 
-class MyComponent extends Component{
-    render() {
-        return <div>
-            <span>span</span>
-            <div>
-                {true}
-                {this.children}
-            </div>
-        </div>
+// class MyComponent extends Component{
+//     render() {
+//         return <div>
+//             <span>span</span>
+//             <div>
+//                 {true}
+//                 {this.children}
+//             </div>
+//         </div>
+//     }
+// }
+class Board extends Component {
+    renderSquare(i) {
+      return <Square value={i} />;
     }
-
+    render() {
+        return (
+          <div>
+            <div className="board-row">
+              {this.renderSquare(0)}
+              {this.renderSquare(1)}
+              {this.renderSquare(2)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(3)}
+              {this.renderSquare(4)}
+              {this.renderSquare(5)}
+            </div>
+            <div className="board-row">
+              {this.renderSquare(6)}
+              {this.renderSquare(7)}
+              {this.renderSquare(8)}
+            </div>
+          </div>
+        );
+    }
+}
+class Square extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            value: null
+        }
+    }
+    render() {
+      return (
+        <button className="square" onClick={() => this.setState({value: 'x'})}>
+          {this.state.value ? this.state.value : ''}
+        </button>
+      );
+    }
 }
 
-let a = <MyComponent name="a">
-    <div>children</div>
-</MyComponent>
+let a = <Board></Board>
 // console.log(a)
 // document.body.appendChild(a)
 
